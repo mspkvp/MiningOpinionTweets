bb4d9d543869b14087b7b8900fd19bb4afc58956
mspkvp <msp_in_2020@hotmail.com>
2015-11-27 16:25:44 +0000

    Is this it? 8GB and 5 minutes to run it

--- src/tfidf_topics.py
+++ src/tfidf_topics.py
@@ -2,29 +2,38 @@ from collections import defaultdict
 import csv
 
 tweets = defaultdict(list)
-with open ("Jose Mourinho_filtered.csv", "r") as tweets_file:
+with open (u"José Mourinho_filtered.csv", "r") as tweets_file:
     reader = csv.reader(tweets_file, delimiter="\t", quotechar='|', quoting=csv.QUOTE_MINIMAL)
     for row in reader:
         tweets[row[0]].append(row[1])
 
 # text comes in a list as ['text'] and we need the 'text' only
-for entity, text in tweets.iteritems():
+for entity, text in tweets.items():
     tweets[entity] = "".join(text)
 
 corpus = []
-for entity, text in sorted(tweets.iteritems(), key=lambda e: entity):
+for entity, text in sorted(tweets.items(), key=lambda e: entity):
     corpus.append(text)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), min_df = 0)
+tf = TfidfVectorizer(analyzer='word', ngram_range=(1,1), min_df = 0)
 
-tfidf_matrix =  tf.fit_transform(corpus)
+tfidf_matrix = tf.fit_transform(corpus)
 feature_names = tf.get_feature_names()
 
-print len(feature_names)
+print(len(feature_names))
 
-dense = tfidf_matrix.todense()
-tweets_entity = dense[0].tolist()[0]
+print(len(tfidf_matrix.toarray()))
+
+# sum values from all documents
+word_vals = [0.0]*len(tfidf_matrix.toarray())
+
+for idx, row in enumerate(tfidf_matrix.toarray()):
+    for col in row:
+        word_vals[idx] = (word_vals[idx] + col)/2.0
+
+# dense = tfidf_matrix.todense()
+tweets_entity = word_vals # tfidf_matrix.toarray()[0] # dense[0].tolist()[0]
 
 phrase_scores = [pair for pair in zip(range(0, len(tweets_entity)), tweets_entity) if pair[1] > 0]
 sorted_phrase_scores = sorted(phrase_scores, key=lambda t: t[1] * -1)
