be8546a8930324bfb024bfe257351bd0dc5f78c7
mspkvp <msp4ever@gmail.com>
2015-12-02 00:54:07 +0000

    tfidf updated, should work as planned, need to confirm results

--- src/tfidf_topics.py
+++ src/tfidf_topics.py
@@ -1,52 +1,62 @@
 from collections import defaultdict
 import csv
 
-tweets = defaultdict(list)
-with open (u"José Mourinho_filtered.csv", "r") as tweets_file:
-    reader = csv.reader(tweets_file, delimiter="\t", quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for row in reader:
-        tweets[row[0]].append(row[1])
 
-# text comes in a list as ['text'] and we need the 'text' only
-for entity, text in tweets.items():
-    tweets[entity] = "".join(text)
+def retrieve_tfidf_scores( array_of_filenames ):
 
-corpus = []
-for entity, text in sorted(tweets.items(), key=lambda e: entity):
-    corpus.append(text)
+    tweets = defaultdict(list)
+    with open (u"Jose Mourinho_filtered.csv", "r") as tweets_file:
+        reader = csv.reader(tweets_file, delimiter="\t", quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row in reader:
+            tweets[row[0]].append(row[1])
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-tf = TfidfVectorizer(analyzer='word', ngram_range=(1,1), min_df = 0)
+    # text comes in a list as ['text'] and we need the 'text' only
+    for entity, text in tweets.items():
+        tweets[entity] = "".join(text)
 
-tfidf_matrix = tf.fit_transform(corpus)
-feature_names = tf.get_feature_names()
+    corpus = []
+    for entity, text in sorted(tweets.items(), key=lambda e: entity):
+        corpus.append(text)
 
-print(len(feature_names))
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    tf = TfidfVectorizer(analyzer='word', ngram_range=(1,1), min_df = 0)
 
-# sum values from all documents
-words_amount = len(tfidf_matrix.toarray()[0])
-word_rating = [0.0]*words_amount
-row_amount = len(tfidf_matrix.toarray())
+    tfidf_matrix = tf.fit_transform(corpus)
+    feature_names = tf.get_feature_names()
 
-print("Amount of Tweets: "+str(row_amount))
-print("Amount of Words: " + str(words_amount))
+    #print("Feature Names: " + str(len(feature_names)))
 
-for row in tfidf_matrix.toarray():
-    for idx, col in enumerate(row):
-        word_rating[idx] += col
+    # sum values from all documents
+    n_features = len(feature_names)
+    word_rating = [0.0]*n_features
+    n_tweets = len(tfidf_matrix.indptr)-1
 
+    from scipy.sparse import coo_matrix
+    cx = coo_matrix(tfidf_matrix)
 
-for idx, val in enumerate(word_rating):
-    word_rating[idx] /= row_amount
+    for i,j,v in zip(cx.row, cx.col, cx.data):
+        word_rating[j] += v
 
-#
-tweets_entity = word_rating
+    #print("Amount of Tweets: "+str(n_tweets))
+    #print("Amount of Words: " + str(n_features))
 
-phrase_scores = [pair for pair in zip(range(0, len(tweets_entity)), tweets_entity) if pair[1] > 0]
-sorted_phrase_scores = sorted(phrase_scores, key=lambda t: t[1] * -1)
+    for idx, val in enumerate(word_rating):
+        word_rating[idx] /= n_tweets
 
-for phrase, score in [(feature_names[word_id], score) for (word_id, score) in sorted_phrase_scores]:
-    print(u'{0: <25} {1}'.format(phrase,score))
+    feature_rate_list = zip (feature_names, word_rating)
+
+    sorted_phrase_scores = sorted(feature_rate_list, key=lambda t: t[1] * -1)
+
+    for f, v in sorted_phrase_scores:
+        print(u'{0: <25} {1}'.format(f, v))
+
+    return sorted_phrase_scores;
+
+#phrase_scores = [pair for pair in zip(range(0, len(word_rating)), word_rating) if pair[1] > 0]
+#sorted_phrase_scores = sorted(feature_rate_list, key=lambda t: t[1] * -1)
+
+#for phrase, score in [(feature_names[word_id], score) for (word_id, score) in sorted_phrase_scores]:
+#    print(u'{0: <25} {1}'.format(phrase,score))
 
 ############################################
 #   DEBUG
