b6d761c61c978720f7a0cdcdd29e378e56c5f936
Joao Oliveira <joaoliveira117@gmail.com>
2015-12-07 11:55:21 +0000

    Changed td-idf to write results to csv

--- src/tfidf_topics.py
+++ src/tfidf_topics.py
@@ -1,6 +1,7 @@
 import os
 import logging
-from collections import defaultdict 
+from collections import defaultdict
+import csv
 
 logging.basicConfig(filename='tfidf_analyzer.log', level=logging.DEBUG)
 def retrieve_tfidf_scores():
@@ -48,12 +49,24 @@ def retrieve_tfidf_scores():
         os.makedirs("./tfidf_scores/")
 
     logging.info("Writing to files...")
+    file = csv.writer(open('tfidf_scores.csv', 'wb'))
+
     for key, value in words_entity_day.iteritems():
         logging.info('\tWriting to {0}'.format(entity_day_key_index[key]))
-        f = open("./tfidf_scores/" + entity_day_key_index[key], 'w')
-        for word, rating in value:
-            f.write(u'{0: <25} {1}\n'.format(word, rating))
-        f.close()
+        #f = open("./tfidf_scores/" + entity_day_key_index[key], 'w')
+        #for word, rating in value:
+        #    f.write(u'{0: <25} {1}\n'.format(word, rating))
+        #f.close()
+        split = entity_day_key_index[key].split(' ')
+        date = split[0]
+        entity_name = split[1].split('.')[0]
+        array_to_write = [entity_name, date]
+
+        for word, rating in value[0:20]:
+            array_to_write.append(word)
+
+        file.writerow(array_to_write)
+
 
     return
 
