cff5ec503349736f180155886fa8f42566d15e8b
Joao Oliveira <joaoliveira117@gmail.com>
2016-01-04 16:50:21 +0000

    Word counter runs for all entities

--- src/count_words.py
+++ src/count_words.py
@@ -16,23 +16,22 @@ for i in os.listdir(os.getcwd() + "/filtered_tweets"):
 
 
 
+for entity_day in entity_day_dict:
+    entity_day_values = entity_day.split(' ', 1)
 
-entity_day = '2014-01-02 jose_mourinho'
-entity_day_values = entity_day.split(' ', 1)
+    if not os.path.exists(write_path + "/" + entity_day_values[0]):
+        os.makedirs(write_path + "/" + entity_day_values[0])
 
-if not os.path.exists(write_path + "/" + entity_day_values[0]):
-    os.makedirs(write_path + "/" + entity_day_values[0])
+    test = entity_day_dict[entity_day]
 
-test = entity_day_dict[entity_day]
+    word_count = dict()
+    tf_vectorizer = CountVectorizer()
 
-word_count = dict()
-tf_vectorizer = CountVectorizer()
+    tf = tf_vectorizer.fit_transform([test])
 
-tf = tf_vectorizer.fit_transform([test])
+    cx = coo_matrix(tf)
+    words = tf_vectorizer.get_feature_names()
+    csv_writer = csv.writer(open(write_path + "/" + entity_day_values[0] + "/" + entity_day_values[1] + ".csv", "wb"))
 
-cx = coo_matrix(tf)
-words = tf_vectorizer.get_feature_names()
-csv_writer = csv.writer(open(write_path + "/" + entity_day_values[0] + "/" + entity_day_values[1] + ".csv", "wb"))
-
-for i,j,v in zip(cx.row, cx.col, cx.data):
-    csv_writer.writerow([words[j], v])
+    for i,j,v in zip(cx.row, cx.col, cx.data):
+        csv_writer.writerow([words[j], v])
